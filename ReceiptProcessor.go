b39ebc474c4fc63c95050736f6dbbd1e567eec18
Brandon Lara <larabrandon408@gmail.com>
2024-02-12 13:05:49 -0800

    FRPC-6 Implemented getPoints()
    Implemented the getPoints Function

--- ReceiptProcessor.go
+++ ReceiptProcessor.go
@@ -4,16 +4,17 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/google/uuid"
 )
 
 type Receipt struct {
-	Retailer     string `json:"retailer`
+	Retailer     string `json:"retailer"`
 	PurchaseDate string `json:"purchaseDate"`
 	PurchaseTime string `json:"purchaseTime"`
 	Total        string `json:"total"`
-	Items        []Item `json:"items`
+	Items        []Item `json:"items"`
 }
 
 type Item struct {
@@ -29,6 +30,8 @@ type PointsResponse struct {
 	Points int `json:"points"`
 }
 
+var receipts = make(map[string]Receipt)
+
 func main() {
 	http.HandleFunc("receipts/process", processReceipts)
 	http.HandleFunc("receipts/{id}/points", getPoints)
@@ -37,13 +40,15 @@ func main() {
 }
 
 func processReceipts(writer http.ResponseWriter, request *http.Request) {
-	var receipts Receipt
+	var receipt Receipt
 	error := json.NewDecoder(request.Body).Decode(&receipts)
 	if error != nil {
 		http.Error(writer, error.Error(), http.StatusBadRequest)
 	}
 
 	id := generateReceiptUUID()
+
+	receipts[id] = receipt
 
 	response := ReceiptIDResponse{ID: id}
 	json.NewEncoder(writer).Encode(response)
@@ -55,11 +60,26 @@ func generateReceiptUUID() string {
 	return newUUID.String()
 }
 
-func getPoints(writer http.ResponseWriter, rquest *http.Request) {
+func getPoints(writer http.ResponseWriter, request *http.Request) {
+	parts := strings.Split(request.URL.Path, "/")
+
+	id := parts[len(parts)-2]
+
+	receipt, found := receipts[id]
+	if !found {
+		http.Error(writer, "Receipt not found", http.StatusNotFound)
+		return
+	}
+
+	points := calculatePointsForReceipt(receipt)
+
+	response := PointsResponse{Points: points}
+
+	json.NewEncoder(writer).Encode(response)
 
 }
 
-func calculatePointsByID(id string) int {
+func calculatePointsForReceipt(receipt Receipt) int {
 
 	return 0
 }
