f2cbf2d36aa5b8c1c6b4eea78dd2978fe503d169
Brandon Lara <larabrandon408@gmail.com>
2024-02-12 20:02:12 -0800

    FRPC-12 Added Dockerfile and comments
    Added Dockerfile and moves in go.mod and go.sum into the working directory.

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+
+FROM golang:1.17-alpine AS builder
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the Go modules files
+COPY go.mod .
+COPY go.sum .
+
+RUN go mod download
+
+COPY ReceiptProcessor.go .
+
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o fetch-project-app .
+
+FROM alpine:latest  
+
+WORKDIR /root/
+
+COPY --from=builder /app/fetch-project-app .
+
+EXPOSE 8080
+
+CMD ["./fetch-project-app"]
--- ReceiptProcessor.go
+++ ReceiptProcessor.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 	"time"
 	"unicode"
-
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
@@ -38,24 +37,27 @@ type PointsResponse struct {
 var receipts = make(map[string]Receipt)
 
 func main() {
+	//Attaching the router to the correct functions. 
 	r := mux.NewRouter()
 	r.HandleFunc("/receipts/process", processReceipts).Methods("POST")
 	r.HandleFunc("/receipts/{id}/points", getPoints).Methods("GET")
 
 	http.Handle("/", r)
+	//Running on port 8080
 	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
 
 func processReceipts(writer http.ResponseWriter, request *http.Request) {
+	//Decode the request body and if an error is thrown return 400
 	var receipt Receipt
 	err := json.NewDecoder(request.Body).Decode(&receipt)
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusBadRequest)
 	}
-
+	//Call to generate unique UUID
 	id := generateReceiptUUID()
-
+	//Map the receipt to ID
 	receipts[id] = receipt
 
 	response := ReceiptIDResponse{ID: id}
@@ -64,20 +66,24 @@ func processReceipts(writer http.ResponseWriter, request *http.Request) {
 }
 
 func generateReceiptUUID() string {
+	//Generates and returns a unique UUID
 	newUUID := uuid.New()
 	return newUUID.String()
 }
 
 func getPoints(writer http.ResponseWriter, request *http.Request) {
+	//Splits the path into parts
 	parts := strings.Split(request.URL.Path, "/")
 
 	id := parts[len(parts)-2]
 
+	//If a receipt is not found throws 404 Not found
 	receipt, found := receipts[id]
 	if !found {
 		http.Error(writer, "No receipt found for that id", http.StatusNotFound)
 		return
 	}
+	//Call to calculate the points for receipt
 	points := calculatePointsForReceipt(receipt)
 
 	response := PointsResponse{Points: points}
@@ -87,24 +93,26 @@ func getPoints(writer http.ResponseWriter, request *http.Request) {
 }
 
 func calculatePointsForReceipt(receipt Receipt) int {
+	
 	points := 0
+	//Adding 1 point for all alphaNumeric characters
 	for _, char := range receipt.Retailer {
 		if unicode.IsLetter(char) || unicode.IsDigit(char) {
 			points++
 		}
 	}
-
+	//Adding 50 points if the total is a round dollar amount with no cents.
 	total, err := strconv.ParseFloat(receipt.Total, 64)
 	if err == nil && math.Mod(total, 1) == 0 {
 		points += 50
 	}
-
+	//Adding 25 points if the total is a multiple of 0.25
 	if err == nil && math.Mod(total, 0.25) == 0 {
 		points += 25
 	}
-
+	//Adding 5 points for every 2 items
 	points += len(receipt.Items) / 2 * 5
-
+	//Adding 0.2 * trimmedItemDescription length rounded
 	for _, item := range receipt.Items {
 		trimLength := len(strings.TrimSpace(item.ShortDescription))
 
@@ -115,18 +123,17 @@ func calculatePointsForReceipt(receipt Receipt) int {
 
 				continue
 			}
-
 			points += int(math.Ceil(price * 0.2))
 
 		}
 	}
-
+	//Adding 6 points if it was purchased on an odd day
 	purchaseDate, err := time.Parse("2006-01-02", receipt.PurchaseDate)
 	if err == nil && purchaseDate.Day()%2 != 0 {
 		points += 6
 
 	}
-
+	//Adding 10 points if it was purchased between 2pm and 4pm
 	purchaseTime, err := time.Parse("15:04", receipt.PurchaseTime)
 	if err == nil && purchaseTime.After(time.Date(0, 1, 1, 14, 0, 0, 0, time.UTC)) && purchaseTime.Before(time.Date(0, 1, 1, 16, 0, 0, 0, time.UTC)) {
 		points += 10
